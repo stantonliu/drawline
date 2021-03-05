e9d8ad44676eac2302545af4dc535134efcdf39c
stantonliu <73355708+stantonliu@users.noreply.github.com>
2021-03-05 15:03:25 +0800

    Update liff.js

--- public/liff.js
+++ public/liff.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655685923-2JrX14za";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
